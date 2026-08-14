e175146a050e1d1644ec37da2dd79c0cf1703c41
Breno Godoy Costa <godoyy25z@gmail.com>
2026-08-14 04:44:46 -0300

    fix: Mais uma alteração para se adequar as regras de deploy da vercel para Nest

--- apps/api/api/[...path].ts
+++ apps/api/api/[...path].ts
@@ -1,1 +1,9 @@
-export { default } from './index';
+import type { IncomingMessage, ServerResponse } from 'node:http';
+import handler from './index';
+
+export default async function catchAllHandler(
+  request: IncomingMessage,
+  response: ServerResponse,
+): Promise<void> {
+  await handler(request, response);
+}
